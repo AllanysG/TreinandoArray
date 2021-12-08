8fdf80135d8deda097421a43ec959f998337663f
Allanys Gasparotto <allanys.gasparotto@requestia.local>
2021-12-08 18:22:04 -0300

    TreinandoArray

---
+++ TReinarArray/TreinandoArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TReinarArray
+{
+    class TreinandoArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Treinando a exibição de valores dentro de Array");
+            Console.WriteLine("Apresente valores e no Final Mudara de Posição");
+            Console.WriteLine(" ");
+
+            int[] Array = null;
+            Array = new int[10];
+
+            for (int i = 0; i < Array.Length; i++){
+                Console.WriteLine("Valor " + i);
+                Array[i] = int.Parse(Console.ReadLine());                
+            }
+          
+            for (int i = 0; i < Array.Length; i++) {
+                Console.WriteLine("Posicao " + i + " valor " + Array[i]);
+            }
+            for (int i = 0; i < Array.Length; i++)
+            {
+                int posicao = 0;
+                posicao = Array[i];
+                Array[i] = Array[Array.Length - i];
+            }
+        }
+    }
+}
